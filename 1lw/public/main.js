f21239718a60c687b0baadfccfefa8c1275706d5
wander122156 <sov48656@mal.ru>
2024-09-11 23:32:57 +0300

    польский калкулятор

---
+++ 1lw/public/main.js
@@ -0,0 +1,7 @@
+const hello = 'Helooooo world!!';
+const bye = 'bye bye';
+console.log(hello);
+console.log(hello);
+console.log(bye);
+console.log(hello);
+//# sourceMappingURL=main.js.map
---
+++ 1lw/src/main.ts
@@ -0,0 +1,6 @@
+const hello: string = 'Helooooo world!!';
+const bye: string = 'bye bye';
+console.log(hello);
+console.log(hello);
+console.log(bye);
+console.log(hello);
